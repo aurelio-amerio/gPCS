7cbe6ed887b7f653e22a9c5e16eedb4968fe050b
“Aurelio <aure.amerio@gmail.com>
2023-06-13 16:11:47 +0200

    wip tests

--- tests/test_gPCS.py
+++ tests/test_gPCS.py
@@ -6,6 +6,34 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 
 
-def test_gPCS():
+def test_get_TS_ranking():
+    assert 1 == 1
+    return
+
+def test_get_CDF():
+    assert 1 == 1
+    return
+
+def test_get_QF_from_TS():
+    assert 1 == 1
+    return
+
+def test_get_QF_ranges_from_TS():
+    assert 1 == 1
+    return
+
+def test_get_TS_from_QF():
+    assert 1 == 1
+    return
+
+def test_get_4FGL_source_pixels():
+    assert 1 == 1
+    return
+
+def test_get_firing_pixels():
+    assert 1 == 1
+    return
+
+def test_export_fits_table():
     assert 1 == 1
     return
